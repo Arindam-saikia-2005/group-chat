d75036e2b7a832c22419fb8bc2677205d88783ed
Arindam saikia <arindamsaikia50a@gmail.com>
2026-03-02 08:44:26 +0530

    adding message controller

---
+++ backend/src/routes/message.route.ts
@@ -0,0 +1,8 @@
+import express from "express";
+import { protect } from "../middleware/auth.middleware.js";
+import { deleteMessage, getMessages } from "../controller/message.controller.js";
+
+export  const messageRouter =  express.Router();
+
+messageRouter.get("/:groupId",protect,getMessages);
+messageRouter.get("/:messageId",protect,deleteMessage);
